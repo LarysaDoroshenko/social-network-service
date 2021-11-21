01781e310106db407870547cfcb0cc20524fd0e0
Larysa Doroshenko <larysad785@gmail.com>
2021-11-21 23:08:45 +0200

    init commit

---
+++ src/test/java/com/project/network/NetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
